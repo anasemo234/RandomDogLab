7ceb14c3d59a310f371992d8e26758fc9cc44d49
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-09 22:31:11 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import DisplayDog from './components/DisplayDog';
+import './App.css';
+
+function App() {
+  const API_URL = 'https://dog.ceo/api/breeds/image/random'
+
+  const [dog, setDog] = useState(null)
+
+const getDog = async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  setDog(data.message)
+}
+
+
+
+  return (
+    <div className="App">
+     <h1>Random Dog Generator</h1>
+     <input type="button" value="Get Random Dog" onClick={getDog} />
+     <DisplayDog dog= {dog} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+function Button = (props) => {
+    const handleSubmit = () => {
+        props.handleSubmitFromApp();
+    };
+
+    return (
+        <button type="button" onClick={handleSubmit}>
+                Get Random Dog
+        </button>
+    );
+};
+
+export default Button;
